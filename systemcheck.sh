e826cd62d1875e9fa2f667ccb21393755bb72252
Herb Garcia <herb@3001.io>
2020-08-01 23:18:29 -0500

    Update systemcheck.sh

--- systemcheck.sh
+++ systemcheck.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-FILE=/etc/saildrone/system.check
-if [ -f "$FILE" ]; then
+SYSCHECKED=/etc/saildrone/system.checked
+SYSCHK=/etc/saildrone/system.check
+
+if [ -f "$SYSCHK" ]; then
     logger system.check file exists 
   else
     logger system.check file missing
+fi
+if [ ! -f "SYSCHECKED" ]; then
+   touch /etc/saildrone/system.checked
 fi
 echo $(date "+%Y/%m/%d_%H:%M:%S") > /etc/saildrone/system.checked
